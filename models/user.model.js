8074177b1b6bb68d1fc722c8b44bf54d06efa701
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-25 15:40:44 +0200

    updated user model for auth

--- models/user.model.js
+++ models/user.model.js
@@ -3,9 +3,20 @@ const mongoose = require('mongoose');
 const User = mongoose.model(
     'User',
     new mongoose.Schema({
-        username: String,
-        email: String,
-        password: String,
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required:true,
+        },
         bio: {
             type: String,
             required: false,
