46396629a43e68bcb7dfce36352d75edddda151e
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-25 19:18:22 +0200

    added swagger for users, user get and post

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,104 +1,97 @@
+// Import the necessary controllers
 const { createUser, findAllUsers , findUser, updateUser, deleteUser} = require ('../controllers/user.controller');
 
 /**
  * @swagger
- * components:
- *   schemas:
- *     User:
- *       type: object
- *       properties:
- *         username:
- *           type: string
- *           description: Nom d'utilisateur
- *         email:
- *           type: string
- *           description: Adresse email
- *         password:
- *           type: string
- *           description: Mot de passe
- *         bio:
- *           type: string
- *           description: Biographie de l'utilisateur
- *         role:
- *           type: array
- *           items:
- *             type: string
- *           description: Rôle de l'utilisateur
- *       required:
- *         - username
- *         - email
- *         - password
+ * tags:
+ *   name: User
+ *   description: API de gestion des utilisateurs
  *
- * /user:
+ * definitions:
+ *   User:
+ *     type: object
+ *     properties:
+ *       username:
+ *         type: string
+ *         description: Nom d'utilisateur de l'utilisateur
+ *         example: JohnDoe
+ *       email:
+ *         type: string
+ *         description: Adresse email de l'utilisateur
+ *         example: john.doe@example.com
+ *       password:
+ *         type: string
+ *         description: Mot de passe de l'utilisateur
+ *         example: mysecretpassword
+ *       bio:
+ *         type: string
+ *         description: Biographie de l'utilisateur
+ *         example: I'm a software developer
+ *       role:
+ *         type: array
+ *         description: Rôles de l'utilisateur
+ *         items:
+ *           type: string
+ *           example: user
+ *     required:
+ *       - username
+ *       - email
+ *       - password
+ *
+ * /users:
+ *   get:
+ *     summary: Récupère la liste des utilisateurs
+ *     tags: [User]
+ *     responses:
+ *       '200':
+ *         description: Liste des utilisateurs
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/definitions/User'
  *   post:
  *     summary: Crée un nouvel utilisateur
+ *     tags: [User]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
- *             $ref: '#/components/schemas/User'
+ *             $ref: '#/definitions/User'
  *     responses:
  *       '200':
  *         description: Utilisateur créé avec succès
  *         content:
  *           application/json:
  *             schema:
- *               $ref: '#/components/schemas/User'
+ *               $ref: '#/definitions/User'
  *
  * /user/{id}:
  *   get:
- *     summary: Récupère les informations d'un utilisateur
+ *     summary: Récupérer un utilisateur par son identifiant.
+ *     description: Récupère un utilisateur existant en utilisant son identifiant.
+ *     tags: [User]
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
- *         description: ID de l'utilisateur à récupérer
- *         schema:
- *           type: string
+ *         description: Identifiant de l'utilisateur à récupérer.
+ *         type: string
  *     responses:
- *       '200':
- *         description: Utilisateur récupéré avec succès
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *   patch:
- *     summary: Met à jour un utilisateur existant
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID de l'utilisateur à mettre à jour
+ *       200:
+ *         description: Utilisateur récupéré avec succès.
  *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *     responses:
- *       '200':
- *         description: Utilisateur mis à jour avec succès
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/User'
- *   delete:
- *     summary: Supprime un utilisateur existant
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         description: ID de l'utilisateur à supprimer
- *         schema:
- *           type: string
- *     responses:
- *       '200':
- *         description: Utilisateur supprimé avec succès
+ *           $ref: "#/definitions/User"
+ *       404:
+ *         description: Utilisateur non trouvé - L'identifiant fourni ne correspond à aucun utilisateur existant.
+ *       500:
+ *         description: Erreur lors de la récupération de l'utilisateur.
+ *
  */
 
+// Export the routes to be used by the app
 module.exports = (app) => {
     app.post('/user', createUser);
     app.get('/users', findAllUsers);
