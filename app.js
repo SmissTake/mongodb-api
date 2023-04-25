0e7ddc374a6ced908057d1d7aad2e99fa5f1efd2
Gwion Tangi <lil.smiss7@gmail.com>
2023-04-25 10:31:24 +0200

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const db = require('./models/index');
 const  { add, findAll } = require('./controllers/place.controller');
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 db.mongoose.connect('mongodb://localhost:27017/Test');
