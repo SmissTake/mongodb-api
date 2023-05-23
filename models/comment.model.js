75f221df2b4dabbdf3a4c7b4f45df7f7c778b6db
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-23 11:26:21 +0200

    updated model schema

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Comment = mongoose.model(
-    'Comment',
-    new mongoose.Schema({
-        comment: String,
-        created_at: Date,
-        user: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'User'
-            }
-        ],
-        image: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Image'
-            }
-        ],
-        place: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Place'
-            }
-        ],
-    })
-);
+const Comment = new mongoose.Schema({
+  comment: String,
+  created_at: Date,
+  user: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  image: [
+    {
+      url: String,
+    },
+  ],
+  place: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Place",
+    },
+  ],
+});
 
 module.exports = Comment;
