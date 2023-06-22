2afaa938c51501a6ef17c6ae2a471125a2172a5e
Gwion Tangi <lil.smiss7@gmail.com>
2023-06-22 10:15:01 +0200

    updated validation for user

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -1,12 +1,14 @@
 const Joi = require('joi');
 
 const registerSchema = Joi.object({
+    username: Joi.string().required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(6).required(),
+    bio: Joi.string().allow('')
 });
 
 const loginSchema = Joi.object({
-    email: Joi.string().email().required(),
+    username: Joi.string().required(),
     password: Joi.string().min(6).required()
 });
 
